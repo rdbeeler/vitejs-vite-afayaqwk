f7095228ffb1ce86a7cc14d768b4cb7e4f73dcbf
rdbeeler <rdbeeler@cps.edu>
2026-08-03 13:43:55 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react' // Fixed: / instead of .
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vitejs-vite-afayaqwk/',
+  base: '/enzyme-viewer-tweaks/',
 })
