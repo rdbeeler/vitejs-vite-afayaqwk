310e22f652c54e158a7b822f0439df397d52c496
rdbeeler <rdbeeler@cps.edu>
2026-08-03 13:14:09 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs.plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vitejs-vite-ukitnfuz/', // MUST have a slash at the start and end!
+  base: '/vitejs-vite-ukitnfuz/', // Exact repo name
 })
